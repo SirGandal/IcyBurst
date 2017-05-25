573238f97c8ee5f3996fb20cdade99333e6464dc
Sergio Andaloro <andalorosergio@gmail.com>
2017-05-25 07:37:53 -0700

    Fixing images filepath on Windows.

--- helper_scripts/PrePopulateImagesText.py
+++ helper_scripts/PrePopulateImagesText.py
@@ -12,7 +12,7 @@ if not os.path.isdir(photoFolderName):
 	sys.exit()
 	
 print "Scanning directory for images..."
-images = [join(photoFolderName, filename) for filename in os.listdir(photoFolderName) if isfile(join(photoFolderName, filename)) and not filename.startswith(".")]
+images = [folder+"/"+filename for filename in os.listdir(photoFolderName) if isfile(join(photoFolderName, filename)) and not filename.startswith(".")]
 print str(len(images)) + " images found."
 
 with open(photoFolderName + '.csv', 'wb') as csvfile:
